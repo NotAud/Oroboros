e36fa01c954d173696d3e6ccbcaf2d46a842b01f
Ron Miller <rmiller232501@gmail.com>
2024-04-09 23:19:45 -0400

    UI Changes and reset button on hotkey

--- src/components/Settings/Hotkey.vue
+++ src/components/Settings/Hotkey.vue
@@ -117,17 +117,36 @@ async function saveHotkey() {
 </script>
 
 <template>
-  <button
-    ref="hotkeyButton"
-    class="px-4 py-1 min-w-[140px] border rounded-md hover:scale-105 transition-all"
-    :class="isCapturing ? 'border-red-800 scale-105' : ''"
-    @click="toggleRecording"
-    @keydown="handleHotkeyPress"
-    @keyup="handleHotkeyRelease"
-    @focusout="abortRecording"
-  >
-    {{ hotkeyDisplay }}
-  </button>
+  <div class="flex gap-x-2 items-center">
+    <button
+      v-if="settingsStore.settings.hotkey.length > 0"
+      class="h-full px-2 hover:scale-105 transition-all"
+      @click="resetRecording"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="18"
+        w="18"
+        viewBox="0 0 512 512"
+        class="fill-white"
+      >
+        <path
+          d="M386.3 160H336c-17.7 0-32 14.3-32 32s14.3 32 32 32H464c17.7 0 32-14.3 32-32V64c0-17.7-14.3-32-32-32s-32 14.3-32 32v51.2L414.4 97.6c-87.5-87.5-229.3-87.5-316.8 0s-87.5 229.3 0 316.8s229.3 87.5 316.8 0c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0c-62.5 62.5-163.8 62.5-226.3 0s-62.5-163.8 0-226.3s163.8-62.5 226.3 0L386.3 160z"
+        />
+      </svg>
+    </button>
+    <button
+      ref="hotkeyButton"
+      class="px-4 py-1 min-w-[140px] border rounded-md select-none hover:scale-105 transition-all"
+      :class="isCapturing ? 'border-red-800 scale-105' : ''"
+      @click="toggleRecording"
+      @keydown="handleHotkeyPress"
+      @keyup="handleHotkeyRelease"
+      @focusout="abortRecording"
+    >
+      {{ hotkeyDisplay }}
+    </button>
+  </div>
 </template>
 
 <style scoped></style>
--- src/components/Settings/LockableWindows.vue
+++ src/components/Settings/LockableWindows.vue
@@ -36,19 +36,20 @@ async function set_window(windowInfo: [number, string]) {
       v-if="settingsStore.settings.windowDetection"
       class="flex items-center gap-x-4"
     >
-      <div class="flex items-end h-full relative">
+      <div class="flex h-full relative">
         <button
-          class="border border-white rounded-md bg-zinc-800/5 py-0.5 hover:bg-zinc-900/80 transition-all w-[200px] whitespace-nowrap text-ellipsis overflow-x-hidden"
+          class="rounded-md bg-white/5 py-0.5 hover:bg-white/10 transition-all shd w-[200px] whitespace-nowrap text-ellipsis overflow-x-hidden"
+          :class="isOpen ? 'bg-white/10' : ''"
           @click="isOpen = !isOpen"
         >
           {{ displayName }}
         </button>
         <div
           v-if="isOpen"
-          class="flex flex-col bg-zinc-900 overflow-x-hidden max-h-[300px] max-w-full rounded-[4px] overflow-y-auto absolute top-[calc(100%+5px)] left-0 z-10 shadow-md transition-all"
+          class="flex flex-col bg-white/5 overflow-x-hidden max-h-[200px] max-w-full rounded-[4px] overflow-y-auto absolute top-[calc(100%+5px)] left-0 z-10 shadow-md transition-all"
         >
           <button
-            class="px-2 py-1.5 flex-none text-start hover:bg-zinc-950/40 transition-all whitespace-nowrap text-ellipsis overflow-x-hidden"
+            class="px-2 py-1.5 flex-none text-start hover:bg-white/5 transition-all whitespace-nowrap text-ellipsis overflow-x-hidden"
             v-for="(window, $i) in settingsStore.settings.lockableWindows"
             :key="$i"
             @click="set_window(window)"
@@ -61,25 +62,4 @@ async function set_window(windowInfo: [number, string]) {
   </div>
 </template>
 
-<style scoped>
-/* width */
-::-webkit-scrollbar {
-  width: 5px;
-}
-
-/* Track */
-::-webkit-scrollbar-track {
-  background: transparent;
-}
-
-/* Handle */
-::-webkit-scrollbar-thumb {
-  @apply rounded-full;
-  background: #888;
-}
-
-/* Handle on hover */
-::-webkit-scrollbar-thumb:hover {
-  background: #555;
-}
-</style>
+<style scoped></style>
